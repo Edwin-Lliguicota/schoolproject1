6e1749f0865fb9c2d007d055c14b7424bfd49d6f
Edwin-Lliguicota <edwin.lliguicota062@gmail.com>
2024-02-25 20:36:11 -0500

    backend

--- backend/Server.js
+++ backend/Server.js
@@ -2,11 +2,14 @@
 const express = require('express');
 const Path = require('path');
 const WebsiteRoutes = require('./routes/routes.js')
-
-
 const App = express()
 const PORT = 3000;
 
+// View Engine SET
+App.set('view engine', 'ejs');
+App.set('views', Path.join(__dirname, "../"))
+
+// Website Routes INSERT
 App.use(WebsiteRoutes)
 
 App.listen(PORT, () => console.log(`Server Running: http://localhost:${PORT}`))
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const Router = express.Router()
-
+var object = {
+  hello:"hello"
+}
 Router.get('/', (req, res) => {
   res.send("Testing!")
-})
 
+})
+Router.get('/test',(req,res)=> {
+  res.send(object)
+})
 module.exports = Router;
